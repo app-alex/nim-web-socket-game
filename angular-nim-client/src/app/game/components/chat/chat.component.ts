a1defab021713872bba3ab379770e0bb4fb1c4c2
alex-app <alexandru.apostol99@gmail.com>
2023-01-16 00:34:04 +0200

    Style chat component

--- angular-nim-client/src/app/game/components/chat/chat.component.ts
+++ angular-nim-client/src/app/game/components/chat/chat.component.ts
@@ -1,18 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewChecked {
   public message = '';
 
   @Input() public messages!: string[] | null;
   @Output() public sendMessage = new EventEmitter<string>();
+  @ViewChild('chatMessages') private chatMessagesRef!: ElementRef;
+
+  public ngAfterViewChecked(): void {
+    this.scrollChat();
+  }
 
   public onSendMessage() {
     this.sendMessage.emit(this.message);
     this.message = '';
+    this.scrollChat();
+  }
+
+  private scrollChat() {
+    this.chatMessagesRef.nativeElement.scrollTop =
+      this.chatMessagesRef.nativeElement.scrollHeight;
   }
 }
