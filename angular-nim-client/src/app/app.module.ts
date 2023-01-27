ec1ac7fd86951315feed0e223378810a90b5ea17
alex-app <alexandru.apostol99@gmail.com>
2023-01-28 00:27:34 +0200

    Remove environment

--- angular-nim-client/src/app/app.module.ts
+++ angular-nim-client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
 import { ChatComponent } from './game/components/chat/chat.component';
 import { RoomsComponent } from './rooms/rooms.component';
@@ -14,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 const config: SocketIoConfig = {
-  url: environment.SERVER_URL,
+  url: 'https://nodejs-nim-server-web-socket.adaptable.app',
   options: { transports: ['websocket'], upgrade: false },
 };
 @NgModule({
